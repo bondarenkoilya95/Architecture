754764cc7ba115baa554e511a0aa1c56f939b993
Ilya Bondarenko <bondarenko@dnt-lab.com>
2022-12-04 00:15:13 +0200

    Implemented Scenario component. Added BaseScenario and InitialScenario

---
+++ Sources/Architecture/Scenarios/InitialScenario.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ilya Bondarenko on 04.12.2022.
+//
+
+import Foundation
+import UIKit
+
+public class InitialScenario: Scenario {
+    
+    public private(set) var window: UIWindow
+    
+    // MARK: - Init Method
+    
+    public init(delegate: ScenarioDelegate? = nil, window: UIWindow) {
+        self.window = window
+        super.init(delegate: delegate)
+    }
+    
+    // MARK: - Setup Root View Controller
+    
+    public func setup(rootViewController: UIViewController) {
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
+}
